766daaa5762fd006437e0a6cc551fa84ccb1890d
Gavin Morrow <gavinfmorrow@gmail.com>
2023-07-09 12:46:06 -0400

    remake document based app, fix stuff

--- sortf/ContentView.swift
+++ sortf/ContentView.swift
@@ -2,17 +2,14 @@
 //  ContentView.swift
 //  sortf
 //
-//  Created by Gavin on 7/7/23.
+//  Created by Gavin on 7/9/23.
 //
 
 import SwiftUI
-import SwiftData
-import Photos
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
     @Binding var document: SortfDocument
-    
+
     var body: some View {
         TextEditor(text: $document.text)
     }
--- sortf/SortfApp.swift
+++ sortf/SortfApp.swift
@@ -1,15 +1,14 @@
 //
-//  sortfApp.swift
+//  SortfApp.swift
 //  sortf
 //
-//  Created by Gavin on 7/7/23.
+//  Created by Gavin on 7/9/23.
 //
 
 import SwiftUI
-import SwiftData
 
 @main
-struct sortfApp: App {
+struct SortfApp: App {
     var body: some Scene {
         DocumentGroup(newDocument: SortfDocument()) { file in
             ContentView(document: file.$document)
